b3b919ce40feb8fbdea6902f339a5601bb5794c8
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-04-30 14:45:27 -0700

    passing State to Child

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
+import React, { useState } from "react";
 
+import Counter from "./components/counter";
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Total Count:{count} </h1>
+      <Counter name="fang-wen" setCount={setCount} count={count} />
+      <Counter name="Hsi-wen" setCount={setCount} count={count} />
     </div>
   );
 }
